ed4dc49d2c75c4f675a857b41df52ca876a2f5cc
Monke001 <rharanru@gmail.com>
2021-09-06 06:15:50 -0300

    change zip file name, add error treatment, not save duplicates code add

--- images/views.py
+++ images/views.py
@@ -20,64 +20,73 @@ class ImagemView(View):
         soup = BeautifulSoup(page.content, 'html.parser')
         images = soup.find_all('img')
         images_figure = soup.find_all('figure')
+
         lista_imagens = []
         lista_imagens_names = []
 
-        for i in images_figure:
-            data = 'src'
-            if i.img[data][-4:] != '.svg' and '.' in i.img[data][-4:]:
+        try:
+            for i in images_figure:
                 data = 'src'
-            else:
-                data = 'data-orig-file'
-            if i.img:
-                print(str(i.img[data]))
                 if i.img[data][-4:] != '.svg' and '.' in i.img[data][-4:]:
-                    print(i.img[data])
-                    lista_imagens.append(i.img[data])
+                    data = 'src'
+                else:
+                    data = 'data-orig-file'
+                if i.img:
+                    print(str(i.img[data]))
+                    if i.img[data][-4:] != '.svg' and '.' in i.img[data][-4:]:
+                        if i.img[data] not in lista_imagens:
+                            lista_imagens.append(i.img[data])
 
-                    url = i.img[data]
-                    filename = url.split('/')[-1]
-                    r = requests.get(url, allow_redirects=True)
-                    save_path = os.path.abspath("media/all_images")
-                    complete = os.path.join(save_path, filename)
+                        url = i.img[data]
+                        filename = url.split('/')[-1]
+                        r = requests.get(url, allow_redirects=True)
+                        save_path = os.path.abspath("media/all_images")
+                        complete = os.path.join(save_path, filename)
 
-                    try:
-                        open(complete, 'wb').write(r.content)
-                    except Exception as erro:
-                        print(erro)
+                        try:
+                            open(complete, 'wb').write(r.content)
+                        except Exception as erro:
+                            print(erro)
+                        if f'/media/all_images/{filename}' not in lista_imagens_names:
+                            lista_imagens_names.append(f'/media/all_images/{filename}')
+        except Exception as erro:
+            print(erro)
 
-                    save_all = ImagensModel.objects.create(title=filename, img=complete)
-                    lista_imagens_names.append(f'/media/all_images/{filename}')
-
-        for i in images:
-            data = 'src'
-            if str(i.get(data))[-4:] != '.svg' and '.' in str(i.get(data))[-4:]:
+        try:
+            for i in images:
                 data = 'src'
-            else:
-                data = 'data-orig-file'
-            if i.get(data):
                 if str(i.get(data))[-4:] != '.svg' and '.' in str(i.get(data))[-4:]:
-                    print(i.get(data))
-                    lista_imagens.append(i.get(data))
+                    data = 'src'
+                else:
+                    data = 'data-orig-file'
+                if i.get(data):
+                    if str(i.get(data))[-4:] != '.svg' and '.' in str(i.get(data))[-4:]:
+                        if i.get(data) not in lista_imagens:
+                            lista_imagens.append(i.get(data))
 
-                    url = str(i.get(data))
-                    filename = url.split('/')[-1]
-                    r = requests.get(url, allow_redirects=True)
-                    save_path = os.path.abspath("media/all_images")
-                    complete = os.path.join(save_path, filename)
+                        url = str(i.get(data))
+                        filename = url.split('/')[-1]
+                        r = requests.get(url, allow_redirects=True)
+                        save_path = os.path.abspath("media/all_images")
+                        complete = os.path.join(save_path, filename)
 
-                    try:
-                        open(complete, 'wb').write(r.content)
-                    except Exception as erro:
-                        print(erro)
+                        try:
+                            open(complete, 'wb').write(r.content)
+                        except Exception as erro:
+                            print(erro)
 
-                    save_all = ImagensModel.objects.create(title=filename, img=complete)
-                    lista_imagens_names.append(f'/media/all_images/{filename}')
+                        if f'/media/all_images/{filename}' not in lista_imagens_names:
+                            lista_imagens_names.append(f'/media/all_images/{filename}')
+        except Exception as erro:
+            print(erro)
 
-        with zipfile.ZipFile(f'{os.path.abspath("media/all_images")}/final.zip', 'w') as zipF:
-            for file in lista_imagens:
-                zipF.write(f"media/all_images/{file.split('/')[-1]}", compress_type=zipfile.ZIP_DEFLATED)
-            zipF.close()
+        try:
+            with zipfile.ZipFile(f'{os.path.abspath("media/all_images")}/all_images.zip', 'w') as zipF:
+                for file in lista_imagens:
+                    zipF.write(f"media/all_images/{file.split('/')[-1]}", compress_type=zipfile.ZIP_DEFLATED)
+                zipF.close()
+        except Exception as erro:
+            print(erro)
 
         print(lista_imagens_names)
         msg = f'{len(lista_imagens)} Imagens encontradas em {rota}'
@@ -86,7 +95,7 @@ class ImagemView(View):
             'post': post,
             'img_total': imagens_total,
             'img_name': lista_imagens_names,
-            'tudo': '/media/all_images/final.zip',
+            'tudo': '/media/all_images/all_images.zip',
             'msg': msg,
             'site_status': page,
         }
