e2e4f0eb2af2ac4002acd897c22db65dea942da4
Monke001 <rharanru@gmail.com>
2021-09-05 22:17:41 -0300

    gunicorn and whitenoise

--- images/views.py
+++ images/views.py
@@ -19,15 +19,23 @@ class ImagemView(View):
         page = requests.get(rota)
         soup = BeautifulSoup(page.content, 'html.parser')
         images = soup.find_all('img')
+        images_figure = soup.find_all('figure')
         lista_imagens = []
         lista_imagens_names = []
 
-        for i in images:
-            if i.get('src'):
-                if str(i.get('src'))[-4:] != '.svg' and '.' in str(i.get('src'))[-4:]:
-                    lista_imagens.append(i.get('src'))
+        for i in images_figure:
+            data = 'src'
+            if i.img[data][-4:] != '.svg' and '.' in i.img[data][-4:]:
+                data = 'src'
+            else:
+                data = 'data-orig-file'
+            if i.img:
+                print(str(i.img[data]))
+                if i.img[data][-4:] != '.svg' and '.' in i.img[data][-4:]:
+                    print(i.img[data])
+                    lista_imagens.append(i.img[data])
 
-                    url = str(i.get('src'))
+                    url = i.img[data]
                     filename = url.split('/')[-1]
                     r = requests.get(url, allow_redirects=True)
                     save_path = os.path.abspath("media/all_images")
@@ -41,22 +49,30 @@ class ImagemView(View):
                     save_all = ImagensModel.objects.create(title=filename, img=complete)
                     lista_imagens_names.append(f'/media/all_images/{filename}')
 
-            if str(i.get('original-src'))[-4:] != '.svg' and '.' in str(i.get('original-src'))[-4:]:
-                lista_imagens.append(i.get('original-src'))
+        for i in images:
+            data = 'src'
+            if str(i.get(data))[-4:] != '.svg' and '.' in str(i.get(data))[-4:]:
+                data = 'src'
+            else:
+                data = 'data-orig-file'
+            if i.get(data):
+                if str(i.get(data))[-4:] != '.svg' and '.' in str(i.get(data))[-4:]:
+                    print(i.get(data))
+                    lista_imagens.append(i.get(data))
 
-                url = str(i.get('original-src'))
-                filename = url.split('/')[-1]
-                r = requests.get(url, allow_redirects=True)
-                save_path = os.path.abspath("media/all_images")
-                complete = os.path.join(save_path, filename)
+                    url = str(i.get(data))
+                    filename = url.split('/')[-1]
+                    r = requests.get(url, allow_redirects=True)
+                    save_path = os.path.abspath("media/all_images")
+                    complete = os.path.join(save_path, filename)
 
-                try:
-                    open(complete, 'wb').write(r.content)
-                except Exception as erro:
-                    print(erro)
+                    try:
+                        open(complete, 'wb').write(r.content)
+                    except Exception as erro:
+                        print(erro)
 
-                save_all = ImagensModel.objects.create(title=filename, img=complete)
-                lista_imagens_names.append(f'/media/all_images/{filename}')
+                    save_all = ImagensModel.objects.create(title=filename, img=complete)
+                    lista_imagens_names.append(f'/media/all_images/{filename}')
 
         with zipfile.ZipFile(f'{os.path.abspath("media/all_images")}/final.zip', 'w') as zipF:
             for file in lista_imagens:
