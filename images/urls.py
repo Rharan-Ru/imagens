c49e3d7996cdc303bc2f9d048aba0187c642aa28
Monke001 <rharanru@gmail.com>
2021-10-20 07:26:16 -0300

    improve search images function

--- images/urls.py
+++ images/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ImagemView
 
 urlpatterns = [
-    path('', ImagemView.as_view()),
+    path('', ImagemView.as_view(), name='images'),
 ]
--- images/views.py
+++ images/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import ImagensModel
 from bs4 import BeautifulSoup
 import requests
 import os
 from django.conf import settings
+import json
 import zipfile
 
 
@@ -14,27 +15,13 @@ class ImagemView(View):
 
     def post(self, request, *args, **kwargs):
         rota = request.POST['site']
-        post = ImagensModel.objects.all()
 
-        headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Mobile Safari/537.36 Edg/93.0.961.38'}
-        headers2 = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
-        headers3 = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
-        headers4 = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+        headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko)'
+                                 ' Chrome/93.0.4577.63 Mobile Safari/537.36 Edg/93.0.961.38'}
 
-        list_headers = [headers, headers2, headers3, headers4]
-        proxies = {'https://': '45.70.195.77:5678'}
         page = requests.get(rota, headers=headers)
 
-        for header in list_headers:
-            page = requests.get(rota, headers=header)
-            if '200' in str(page):
-                page = requests.get(rota)
-                break
-            else:
-                continue
-
         soup = BeautifulSoup(page.content, 'html.parser')
-        sounds = soup.findAll('')
         images = soup.findAll('img')
 
         lista_imagens = []
@@ -66,12 +53,30 @@ class ImagemView(View):
                     lista_imagens.append(i.get(data))
 
                 filename = url.split('/')[-1]
-                r = requests.get(url, allow_redirects=True)
+                try:
+                    r = requests.get(url, allow_redirects=True)
+                except Exception as erro:
+                    print(erro)
+                    continue
                 save_path = os.path.abspath("media/all_images")
                 complete = os.path.join(save_path, filename)
-
+                num_jpg = complete.find('jpg')
+                num_png = complete.find('png')
+                num_gif = complete.find('gif')
+                num_svg = complete.find('svg')
+                complete_all = ''
+                if num_png != -1:
+                    complete_all = complete[0: num_png + 3]
+                elif num_jpg != -1:
+                    complete_all = complete[0: num_jpg + 3]
+                elif num_gif != -1:
+                    complete_all = complete[0: num_gif + 3]
+                elif num_svg != -1:
+                    complete_all = complete[0: num_svg + 3]
+                else:
+                    continue
                 try:
-                    open(complete, 'wb').write(r.content)
+                    open(complete_all, 'wb').write(r.content)
                 except Exception as erro:
                     print(erro)
 
@@ -89,9 +94,7 @@ class ImagemView(View):
         msg = f'{len(lista_imagens)} Imagens encontradas em {rota}'
         imagens_total = zip(lista_imagens, lista_imagens_names)
         context = {
-            'post': post,
             'img_total': imagens_total,
-            'img_name': lista_imagens_names,
             'tudo': '/media/all_images/all_images.zip',
             'msg': msg,
             'site_status': page,
